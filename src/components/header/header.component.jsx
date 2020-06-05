f93a3d1c46281aa49600d042b342d2a002fa4546
Rajat <rajatarya199zx@gmail.com>
2020-06-05 16:10:46 +0530

    chap 13

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {connect } from 'react-redux';
+import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
 
@@ -33,10 +33,8 @@ const Header = ({ currentUser }) => (
   </div>
 );
 
-const mapStateToProps=(state)=>({
- 
-  currentUSer:state.user.currentUSer
-})
-
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
 
 export default connect(mapStateToProps)(Header);
